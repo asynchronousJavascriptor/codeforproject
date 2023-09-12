7ab7fd94a6fcd2c0ecbf0d2959bbaf6a75abcb69
Harsh Vandana Sharma <harshu8545@gmail.com>
2023-09-12 17:37:24 +0530

    commit of 12september

--- readme.md
+++ readme.md
@@ -7,3 +7,15 @@ delete post
 comment
 bio
 scrolling waala copied code samjho
+
+sabse pahle default image def.png images/upload folder me rakhdo
+har user ke liye schema mein def.png image ko default image set karo
+profile ejs mein image sahi path par show karo'
+ek pencil icon banao on dp
+ek form banao with a input type file
+form ko display none karo
+pencil pe click par input par click karo
+input pe change par form ko submit karo
+ab route hit karo /upload and uske andar saara code likho upload.single waala
+loggedin user ke image filed ki value req.file.filename ke baraabar kardo
+aur loggedinuser ko save krdo
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,21 @@ var router = express.Router();
 const userModel = require("./users");
 const postModel = require("./posts");
 const passport = require('passport');
+const path = require("path");
+const fs = require("fs");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images/uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + path.extname(file.originalname);
+    cb(null, uniqueSuffix)
+  }
+})
+
+const upload = multer({ storage: storage })
 
 const localStrategy = require("passport-local");
 passport.use(new localStrategy(userModel.authenticate()));
@@ -11,66 +26,82 @@ router.get('/', function (req, res, next) {
   res.render('index');
 });
 
+router.post('/upload', isLoggedIn, upload.single("image"), function (req, res, next) {
+  // upload ho chuki hai data req.file mein hai
+  userModel
+    .findOne({ username: req.session.passport.user })
+    .then(function (founduser) {
+      if (founduser.image !== 'def.png') {
+        fs.unlinkSync(`./public/images/uploads/${founduser.image}`);
+      }
+      founduser.image = req.file.filename;
+      founduser.save()
+      .then(function () {
+        res.redirect("back");
+      })
+    });
+});
+
 router.get('/profile', isLoggedIn, function (req, res, next) {
   userModel
-  .findOne({ username: req.session.passport.user })
-  .populate("posts")
-  .then(function (foundUser) {
-    console.log(foundUser);
-    res.render("profile", { foundUser })
-  })
+    .findOne({ username: req.session.passport.user })
+    .populate("posts")
+    .then(function (foundUser) {
+      console.log(foundUser);
+      res.render("profile", { foundUser })
+    })
 });
 
 router.get('/like/:postid', isLoggedIn, function (req, res, next) {
   userModel
-  .findOne({username: req.session.passport.user})
-  .then(function(user){
-    postModel
-    .findOne({_id: req.params.postid})
-      .then(function(post){
-        if(post.likes.indexOf(user._id) === -1){
-          post.likes.push(user._id);
-        }
-        else{
-          post.likes.splice(post.likes.indexOf(user._id), 1);
-        }
+    .findOne({ username: req.session.passport.user })
+    .then(function (user) {
+      postModel
+        .findOne({ _id: req.params.postid })
+        .then(function (post) {
+          if (post.likes.indexOf(user._id) === -1) {
+            post.likes.push(user._id);
+          }
+          else {
+            post.likes.splice(post.likes.indexOf(user._id), 1);
+          }
 
-        post.save()
-        .then(function(){
-          res.redirect("back");
+          post.save()
+            .then(function () {
+              res.redirect("back");
+            })
         })
-      })
-  })
+    })
 });
 
 router.post('/post', isLoggedIn, function (req, res, next) {
   userModel
-  .findOne({username: req.session.passport.user})
-  .then(function(user){
-    postModel.create({
-      userid: user._id,
-      data: req.body.post
-    })
-    .then(function(post){
-      user.posts.push(post._id);
-      user.save()
-      .then(function(){
-        res.redirect("back");
+    .findOne({ username: req.session.passport.user })
+    .then(function (user) {
+      postModel.create({
+        userid: user._id,
+        data: req.body.post
       })
+        .then(function (post) {
+          user.posts.push(post._id);
+          user.save()
+            .then(function () {
+              res.redirect("back");
+            })
+        })
     })
-  })
 });
 
 router.get('/feed', isLoggedIn, function (req, res, next) {
-  userModel.findOne({username: req.session.passport.user})
-  .then(function(user){
-    postModel
-    .find()
-    .populate("userid")
-      .then(function (allposts) {
-        res.render("feed", { allposts, user });
-      });
-  })
+  userModel.findOne({ username: req.session.passport.user })
+    .then(function (user) {
+      postModel
+        .find()
+        .populate("userid")
+        .then(function (allposts) {
+          res.render("feed", { allposts, user });
+        });
+    })
 });
 
 router.get('/login', function (req, res, next) {
@@ -91,29 +122,29 @@ router.post('/login', passport.authenticate("local", {
 });
 
 router.post('/register', function (req, res, next) {
-  userModel.findOne({username: req.body.username})
-  .then(function(foundUser){
-    if(foundUser){
-      // will run if there is some user
-      res.send("username already exists");
-    }
-    else{
-      // will run if there is no use with same username 
-      var newuser = new userModel({
-        username: req.body.username,
-        age: req.body.age,
-        email: req.body.email,
-        image: req.body.image,
-      });
-    
-      userModel.register(newuser, req.body.password)
-        .then(function (u) {
-          passport.authenticate("local")(req, res, function () {
-            res.redirect("/profile");
-          })
+  userModel.findOne({ username: req.body.username })
+    .then(function (foundUser) {
+      if (foundUser) {
+        // will run if there is some user
+        res.send("username already exists");
+      }
+      else {
+        // will run if there is no use with same username 
+        var newuser = new userModel({
+          username: req.body.username,
+          age: req.body.age,
+          email: req.body.email,
+          image: req.body.image,
         });
-    }
-  });
+
+        userModel.register(newuser, req.body.password)
+          .then(function (u) {
+            passport.authenticate("local")(req, res, function () {
+              res.redirect("/profile");
+            })
+          });
+      }
+    });
 });
 
 
